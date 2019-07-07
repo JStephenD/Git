8300ea4ce46e5850f11450957c1f27fc4daeb77c
JStephenD <stephenfishlmao@gmail.com>
2019-07-07 20:46:17 +0800

    oml

--- DASTAL/src/PRELIM/Node.java
+++ DASTAL/src/PRELIM/Node.java
@@ -60,6 +60,6 @@ public class Node {
                 node.insertAfter(7);
             System.out.println(cnt+" : "+data);
         }
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
