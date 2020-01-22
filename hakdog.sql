d472816594607d52c725ca92e03219934b2af150
JStephenD <47050585+JStephenD@users.noreply.github.com>
2020-01-22 18:06:36 +0800

    Update hakdog.sql

--- hakdog.sql
+++ hakdog.sql
@@ -1,7 +1,7 @@
 USE BillingCollection
 DECLARE @list table (acct varchar(30))
 INSERT INTO @list VALUES ('0111201070'), ('0111201075'), ('0111201045'), ('0111201040'), ('0111201035'),
-		('0111200005'), ('0121200520'), ('0121200530'), ('0411200425'), ('0411200200')
+		('0121200520'), ('0121200530'), ('0411200425'), ('0411200200')
 DECLARE @year int = 2018, 
 		@mt0030 int = 12, @mt6031 int = 11, @mt9061 int = 10, @mt91u int = 9
 
@@ -13,35 +13,31 @@ SELECT DISTINCT
 					FROM Ledgers inn_l
 					WHERE inn_l.Acct_No = out_l.Acct_No
 						AND YEAR(inn_l.Trans_Date) = @year 
-					    AND inn_l.Amount > 0
+						AND inn_l.Amount > 0
 					)),
 	'Days 0-30' = ABS((SELECT SUM(inn_l.Amount)
 					FROM Ledgers inn_l
 					WHERE inn_l.Acct_No = out_l.Acct_No
 						AND YEAR(inn_l.Trans_Date) = @year
 						AND MONTH(inn_l.Trans_Date) = @mt0030
-						And inn_l.Amount < 0
 					)),
 	'Days 60-31' = ABS((SELECT SUM(inn_l.Amount)
 					FROM Ledgers inn_l
 					WHERE inn_l.Acct_No = out_l.Acct_No
 						AND YEAR(inn_l.Trans_Date) = @year
 						AND MONTH(inn_l.Trans_Date) = @mt6031
-						And inn_l.Amount < 0
 					)),
 	'Days 90-61' = ABS((SELECT SUM(inn_l.Amount)
 					FROM Ledgers inn_l
 					WHERE inn_l.Acct_No = out_l.Acct_No
 						AND YEAR(inn_l.Trans_Date) = @year
 						AND MONTH(inn_l.Trans_Date) = @mt9061
-						And inn_l.Amount < 0
 					)),
 	'Days 91+' = ABS((SELECT SUM(inn_l.Amount)
 					FROM Ledgers inn_l
 					WHERE inn_l.Acct_No = out_l.Acct_No
 						AND YEAR(inn_l.Trans_Date) = @year
 						AND MONTH(inn_l.Trans_Date) <= @mt91u
-						AND inn_l.Amount < 0
 					)),
 	'Total Payments' = ABS((SELECT SUM(inn_l.Amount)
 					FROM Ledgers inn_l
